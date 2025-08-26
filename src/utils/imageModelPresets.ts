45ed7aa0f198e26bd5f756cb6b08be97cc0dba2d
lone-cloud <lonecloud604@pm.me>
2025-08-25 18:46:38 -0700

    better presets for <8GB VRAM usage

--- src/utils/imageModelPresets.ts
+++ src/utils/imageModelPresets.ts
@@ -14,7 +14,7 @@ export const IMAGE_MODEL_PRESETS: ImageModelPreset[] = [
     name: 'FLUX.1',
     description: 'FLUX.1 development model with default encoders',
     sdmodel:
-      'https://huggingface.co/bullerwins/FLUX.1-Kontext-dev-GGUF/resolve/main/flux1-kontext-dev-Q4_K_S.gguf?download=true',
+      'https://huggingface.co/bullerwins/FLUX.1-Kontext-dev-GGUF/resolve/main/flux1-kontext-dev-Q3_K_S.gguf?download=true',
     sdt5xxl:
       'https://huggingface.co/camenduru/FLUX.1-dev/resolve/main/t5xxl_fp8_e4m3fn.safetensors?download=true',
     sdclipl:
@@ -28,7 +28,7 @@ export const IMAGE_MODEL_PRESETS: ImageModelPreset[] = [
     name: 'Chroma',
     description: 'Chroma with optimized VAE and shared encoders',
     sdmodel:
-      'https://huggingface.co/silveroxides/Chroma-GGUF/resolve/main/chroma-unlocked-v29/chroma-unlocked-v29-Q4_K_S.gguf?download=true',
+      'https://huggingface.co/silveroxides/Chroma-GGUF/resolve/main/chroma-unlocked-v29/chroma-unlocked-v29-Q3_K_L.gguf?download=true',
     sdt5xxl:
       'https://huggingface.co/camenduru/FLUX.1-dev/resolve/main/t5xxl_fp8_e4m3fn.safetensors?download=true',
     sdclipl:
