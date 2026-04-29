28f8cf68c3cd90ace609ee8d5ad1c33372c7662a
lone-cloud <lonecloud604@pm.me>
2026-04-29 01:34:49 -0700

    preparing for flatpak builds, minor UI touch-ups, dep upgrades

--- src/components/screens/Launch/index.tsx
+++ src/components/screens/Launch/index.tsx
@@ -439,7 +439,7 @@ export const LaunchScreen = ({ onLaunch }: LaunchScreenProps) => {
       </Tabs>
       <div
         style={{
-          borderTop: '1px solid var(--mantine-color-default-border)',
+          borderTop: '1px solid var(--gerbil-divider)',
           padding: 'var(--mantine-spacing-md) 0',
           display: 'flex',
           justifyContent: 'flex-end',
